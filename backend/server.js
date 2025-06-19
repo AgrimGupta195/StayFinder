66381199f090c79ba1d413bd5773e6e82983c99f
Agrim Gupta <agrimgupt195@gmail.com>
2025-06-19 22:52:50 +0530

    fix error

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,9 @@ const cors = require('cors');
 const connectDB = require('./lib/db');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
-const path = require('path');
-
-
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
 const PORT = process.env.PORT || 5000;
 app.use("/api/users", require('./routers/userRouter'));
 app.use("/api/listings", require('./routers/listingRouter'));
